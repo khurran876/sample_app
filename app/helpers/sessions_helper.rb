f6571c17629667b55e6eb4251be85b6645eea186
Khurram Shahzad <khurram876@live.com>
2014-07-08 14:26:46 +0500

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,4 +35,11 @@ module SessionsHelper
      user == current_user
   end
 
+  def signed_in_user
+     unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+     end
+  end
+
 end
